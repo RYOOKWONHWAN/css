45b0ce69fb8b4952178ee0e91e084cbeaa8a5afe
RYOOKWONHWAN <fbrnjsghks@gmail.com>
2022-12-27 15:17:31 +0900

    2022-12-27-1

---
+++ javscriptdemo/js/js_041_checkBox.js
@@ -0,0 +1,27 @@
+document.frm.onsubmit = () => {
+    let admin = document.frm.admin;
+    if (!admin.checked) {
+        alert('약관에 동의해야합니다.');
+        return false;
+    }
+};
+document.frm.allcheck.onclick = function () {
+    console.log(this.checked);
+    // document.frm.subject1.checked = this.checked;
+    // document.frm.subject2.checked = this.checked;
+    // document.frm.subject3.checked = this.checked;
+    let checkgroup = document.querySelectorAll('.checkgroup');
+    console.log(checkgroup.length);
+    // for (let i = 0; i < checkgroup.length; i++) {
+    //     checkgroup[i] = this.checked;
+    // }
+    // for (let element of checkgroup) {
+    //     element.checked = this.checked;
+    // }
+    //ES6 Spread operator : NodeList를 Array로 변경
+    let myThis = this;
+    let iArray = [...checkgroup];
+    iArray.map(function (element) {
+        element.checked = myThis.checked;
+    });
+};
